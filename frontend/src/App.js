119b61ffc47aa622d18516ed177275cccf3a8d02
CameronWild <CameronWild@hotmail.co.nz>
2021-08-02 12:53:58 +1200

    first commit maybe the second maybe

---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () =>
+    <div className="App">
+     <h1> Welcome to CISE - the home of learning, fun and brain sores</h1>
+    </div>
+
+export default App;
